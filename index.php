3df2ac445c77763b3dd2eb53e80d994b56ec8d20
Risbo_M <boris.miletic@etu.unistra.fr>
2026-03-11 10:49:21 +0100

    validate

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Redirection...</title>
+    <meta http-equiv="refresh" content="0; url=./fr/page1-accueil.php">
+    <link rel="stylesheet" href="./css/index.css">
+</head>
+<body>
+    <p>Redirection en cours vers le projet <a href="./fr/page1-accueil.php"> SAE - 105</a>...</p>
+</body>
+</html>
